307e67540ac921bac65734b557974a938b0a2692
Юдин Алексей Михайлович <amyudin6@mts.ru>
2021-07-08 23:33:50 +0300

    feat: create user and admin modules

---
+++ src/app/post-page/post-page.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-page',
+  templateUrl: './post-page.component.html',
+  styleUrls: ['./post-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PostPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
