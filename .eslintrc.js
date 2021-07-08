72132a8f7d00f2a9fc7bf12d8ea2f24f90a1497a
Юдин Алексей Михайлович <amyudin6@mts.ru>
2021-07-08 22:27:38 +0300

    init: init project

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: 'tsconfig.*?.json',
+        createDefaultProgram: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates',
+        'airbnb-angular',
+        'airbnb-typescript/base',
+        'prettier',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase',
+          },
+        ],
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case',
+          },
+        ],
+        'import/order': ['error'],
+        'typescript/class-name-casing': 'off',
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'class-methods-use-this': 'off',
+        'lines-between-class-members': 'off',
+        '@typescript-eslint/lines-between-class-members': 'off',
+        '@typescript-eslint/class-name-casing': 'off',
+      },
+    },
+    {
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {
+        'max-len': ['error', { code: 120 }],
+      },
+    },
+  ],
+};
