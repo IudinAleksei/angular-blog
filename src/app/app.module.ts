59b796c78f79e62377c000ef3bae1da87d2aff32
Aleksei Iudin <iudin_a@yahoo.com>
2021-09-28 00:43:50 +0300

    config: first pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import ruLocale from '@angular/common/locales/ru';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AuthInterceptor } from './shared/auth.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { PostPageComponent } from './post-page/post-page.component';
 import { SharedModule } from './shared/shared.module';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { PostComponent } from './shared/components/post/post.component';
+import { environment } from '../environments/environment';
 
 registerLocaleData(ruLocale, 'ru');
 
@@ -23,7 +25,18 @@ const INTERCEPTOR_PROVIDER: Provider = {
 };
 @NgModule({
   declarations: [AppComponent, HomePageComponent, PostPageComponent, MainLayoutComponent, PostComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent],
 })
